8cbee9735940e2e34eb609bb55b663d3547b8d78
Scarlett Eller <rq6715@gmail.com>
2022-02-22 11:14:27 -0500

    Completed Assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,21 @@
 class Dog
+    def name
+        @name
+    end
 
+    def name=(name)
+        @name = name
+    end
+
+    def breed
+        @breed
+    end
+
+    def breed=(breed)
+        @breed = breed
+    end
 end
+
+dog1 = Dog.new
+dog1.name = "Annabelle"
+dog1.breed = "Golden Retriver"
